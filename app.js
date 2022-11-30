f43c4415af715df7192691b11ed811f6a3570f35
Calvin L <calvin_l04@hotmail.com>
2022-11-30 13:08:39 -0500

    app.js changes

--- app.js
+++ app.js
@@ -13,6 +13,7 @@ var querystring = require('querystring');
 var cookieParser = require('cookie-parser');
 var path = require('path');
 var url = require('url');
+var app = express();
 
 var client_id = '3558fa27e3de4d5cba8c5105a3350c2c'; // Your client id
 var client_secret = '59c46d64eb8c4b608b0020551a2648c5'; // Your secret
@@ -144,6 +145,6 @@ app.get('/refresh_token', function(req, res) {
   });
 });
 
-var port = process.env.PORT || 5000;
-console.log('Listening on 5000');
+var port = process.env.PORT || 3030;
+console.log('Listening on 3030');
 app.listen(port);
